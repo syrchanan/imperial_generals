58719f9b3170b46275b4c08a7d3096124754daff
syrchanan <cdawg004@gmail.com>
2024-09-02 21:42:57 -0400

    fixed apos

--- components/FocusCarousel.tsx
+++ components/FocusCarousel.tsx
@@ -5,7 +5,7 @@ export default function FocusCarousel({ titleSlides }: { titleSlides: TitleSlide
     return (
         <div className="flex flex-col h-fit bg-opacity-80 bg-secondary rounded-2xl p-4 my-4">
             <h3 className="text-center text-lg font-serif p-4 font-bold">
-                <i>Explore the Eras We've Played</i>
+                <i>Explore the Eras We&apos;ve Played</i>
             </h3>
             <Carousel opts={{ loop: true }} className="w-full md:w-4/6 lg:w-3/6 xl:w-2/6 mx-auto">
                 <CarouselContent>
